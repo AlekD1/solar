7d6e768dacbaf2bb63f2efd5476edc98662d05dc
Долобан Алексей <alekdn.work@yandex.ru>
2023-05-06 21:31:01 +0300

    Add files via upload

---
+++ solarmodal/js/LoadPage.js
@@ -0,0 +1,30 @@
+function onReady(callback) {
+    var intervalId = window.setInterval(function () {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+        }
+    }, 3000);
+}
+
+document.getElementById("loadButton").addEventListener("click", function () {
+    setVisible('body', true);
+    setVisible('#loading', false);
+});
+
+document.getElementById("loadButton2").addEventListener("click", function () {
+    window.location.href = '../index.html';
+});
+
+document.getElementById("loadButton2").addEventListener("touchstart", function () {
+    window.location.href = '../index.html';
+});
+
+document.getElementById("loadButton").addEventListener("touchstart", function () {
+    setVisible('body', true);
+    setVisible('#loading', false);
+});
+
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
